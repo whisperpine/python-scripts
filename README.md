0e4e6df5644697256ec1ceb45b51a34d348fcd88
Yusong <yusong.lai@icloud.com>
2025-03-11 23:37:37 +0000

    chore: init

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+
+My python scripts.
---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A Nix-flake-based Python development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f: nixpkgs.lib.genAttrs supportedSystems (system: f { pkgs = import nixpkgs { inherit system; }; });
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            venvDir = ".venv";
+            shellHook = ''
+              echo ":: shellHook starts"
+              uv sync
+              echo ":: shellHook ends"
+            '';
+            packages =
+              with pkgs;
+              [ python312 ]
+              ++ (with pkgs.python312Packages; [
+                venvShellHook
+                uv
+              ]);
+          };
+        }
+      );
+    };
+}
