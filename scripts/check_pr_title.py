e036f027efd7e6e79329db1099e9e5f6dda5f51c
Yusong <yusong.lai@icloud.com>
2025-05-27 13:36:45 +0800

    fix(check_pr_title): absence of error message when PR_TITLE is empty

--- scripts/check_pr_title.py
+++ scripts/check_pr_title.py
@@ -18,39 +18,39 @@ https://github.com/whisperpine/check_pr_title
 MIT License.
 """
 
-import os
-import re
-import sys
-
 
 def print_in_red(text: str) -> None:
     """Print text in red color."""
     print(f"\033[91m{text}\033[0m")
 
 
-def check_character_set(text: str) -> bool:
+def is_ascii(text: str) -> bool:
     """Check if input text contains only ASCII chars."""
+    import re
+
     ascii_chars = re.compile(r"[^\x00-\x7F]")
     non_ascii_chars = ascii_chars.findall(text)
 
     if non_ascii_chars:
-        non_ascii_chars = list(dict.fromkeys(non_ascii_chars))
+        non_ascii_chars = set(dict.fromkeys(non_ascii_chars))
         print_in_red(f"error: non-ASCII chars found: {non_ascii_chars}")
         return False
     return True
 
 
-def check_pr_title():
+def main():
+    import os
+    from sys import exit
+
     ENV_VAR_NAME = "PR_TITLE"
     mr_title = os.environ.get(ENV_VAR_NAME)
-    if mr_title is None:
+    if not mr_title:
         print_in_red(f"error: cannot get env var {ENV_VAR_NAME}")
         exit(1)
 
-    title_valid = check_character_set(mr_title)
-    if not title_valid:
-        sys.exit(1)
+    if not is_ascii(mr_title):
+        exit(1)
 
 
 if __name__ == "__main__":
-    check_pr_title()
+    main()
