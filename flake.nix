0c7ac84d0baf0e3a1bfd85bdd1e866eb30cd9ed0
Yusong <yusonglai64@gmail.com>
2025-11-10 14:11:17 +0800

    chore: print the last modified date of "flake.lock"

--- flake.nix
+++ flake.nix
@@ -21,12 +21,20 @@
         { pkgs }:
         {
           default = pkgs.mkShell {
+            # The Nix packages installed in the dev environment.
             packages = with pkgs; [
               python313
-              uv
+              uv # python package and project manager
             ];
+            # The shell script executed when the environment is activated.
             shellHook = ''
+              # Print the last modified date of "flake.lock".
+              stat flake.lock | grep "Modify" |
+                awk '{printf "\"flake.lock\" last modified on: %s", $2}' &&
+                echo " ($((($(date +%s) - $(stat -c %Y flake.lock)) / 86400)) days ago)"
+              # Install python project dependencies.
               uv sync
+              # Active python virtual environment.
               source .venv/bin/activate
             '';
           };
